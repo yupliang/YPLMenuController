77635b49dad4f19b498a9e572e5f6fa306b9a40f
yupeiliang <yupeiliang@fotoable.com>
2017-11-17 17:19:08 +0800

    add xcode profile

---
+++ YPLMenuController/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  YPLMenuController
+//
+//  Created by yupeiliang on 2017/11/17.
+//  Copyright © 2017年 PL Technology. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ YPLMenuController/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  YPLMenuController
+//
+//  Created by yupeiliang on 2017/11/17.
+//  Copyright © 2017年 PL Technology. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
