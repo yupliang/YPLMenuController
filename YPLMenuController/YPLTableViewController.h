a6fe09a126dd8857843dddb5703299cfb9e04e90
yupeiliang <yupeiliang@fotoable.com>
2017-11-17 18:55:25 +0800

    fill menu table.

---
+++ YPLMenuController/YPLTableViewController.h
@@ -0,0 +1,13 @@
+//
+//  YPLTableViewController.h
+//  YPLMenuController
+//
+//  Created by yupeiliang on 2017/11/17.
+//  Copyright © 2017年 PL Technology. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface YPLTableViewController : UITableViewController
+
+@end
